33beef01f50a49ea1defe12bb89a46a94cc1c494
Deepita Pai <paideepita@gmail.com>
2019-05-04 12:51:01 +0200

    array

--- Leetcode solutions/missing-ranges.py
+++ Leetcode solutions/missing-ranges.py
@@ -3,17 +3,19 @@ def missingRanges(nums,lower,upper):
 	i=0
 	nums.insert(0,lower-1)
 	nums.append(upper+1)
+	print(nums)
 	while i<len(nums)-1:
 		left,right = nums[i],nums[i+1]
 		if left != right-1:
 			if right-left == 2:
 				res.append(str(right-1))
 
-			else:
+			elif right-left >2:
+				print(left,right)
 				res.append(str(left+1)+"->"+str(right-1))
 		i +=1
 	return res
 
 
-nums = [0,1,3,50,75]
-print(missingRanges(nums,0,99))
+nums = [1,1,1]
+print(missingRanges(nums,1,1))
